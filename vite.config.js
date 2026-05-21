537f25e209b4c5780cf63b0f7cec727bb61ff28d
miketan193 <121536147+miketan193@users.noreply.github.com>
2026-05-21 11:10:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/olevel-math-gps/', // Add this line matching your exact repository name!
 })
